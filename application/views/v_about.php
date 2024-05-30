16bc5b49c87e488042b53f3e12e17729f853f2d8
sekar khalillah djau <djausekar03@gmail.com>
2024-05-30 12:16:02 +0700

    Pertemuan 9

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Raifelnavies</li>
+        <li>Nama Belakang : Yuhabfi</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Pepaya 5 No. 69D Jagakarsa</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Bola</li>
+    </ul>
+</section>
